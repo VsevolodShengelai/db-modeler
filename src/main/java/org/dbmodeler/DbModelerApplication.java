6507007ecb345a8b95b27831b925123d30678350
Vsevolod <vshengelai@vtb.ru>
2024-05-06 18:52:56 +0300

    init: Инициализация проекта с помощью Spring initilazr

---
+++ src/main/java/org/dbmodeler/DbModelerApplication.java
@@ -0,0 +1,13 @@
+package org.dbmodeler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbModelerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbModelerApplication.class, args);
+    }
+
+}
